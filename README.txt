ad2a077bb42681dcd4841285166598bad71ce6a5
dpres <thepressu@gmail.com>
2017-09-13 20:48:35 +0200

    start

---
+++ README.txt
@@ -0,0 +1,5 @@
+﻿zadanie 9
+
+1. start 17.04
+2. html/css/js
+3. rwd obowiazkowo
---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+jQuery(window).on("load", function () {
+    jQuery(".preloader").fadeOut();
+});
+/*
+jQuery(document).ready(function () {
+    jQuery('.carousel').carousel({
+        indicators: true,
+        fullWidth: true,
+        duration: 200,
+        shift: 6,
+    });
+    setInterval(function () { $('.carousel').carousel('next'); }, 4000);
+
+    jQuery(".scroll-icon").click(function () {
+        jQuery('html, body').animate({
+            scrollTop: jQuery("#aboutus").offset().top
+        }, 1000, function(){
+            //nothing :P
+        });
+    });
+});
+*/
