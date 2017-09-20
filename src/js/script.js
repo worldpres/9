a74c721312e77e163573f68c88113c6794946b6c
dpres <thepressu@gmail.com>
2017-09-21 00:16:09 +0200

    menu fixed, arrow up and down

--- src/js/script.js
+++ src/js/script.js
@@ -11,13 +11,27 @@ jQuery(document).ready(function () {
     });
     setInterval(function () { $('.carousel').carousel('next'); }, 4000);
 
-    /*
-    jQuery(".scroll-icon").click(function () {
+    jQuery(".arrow_down").click(function () {
         jQuery('html, body').animate({
-            scrollTop: jQuery("#aboutus").offset().top
+            scrollTop: jQuery(".row.menu").offset().top
         }, 1000, function(){
             //nothing :P
         });
     });
-    */
+
+    jQuery(".arrow_up").click(function () {
+        jQuery('html, body').animate({
+            scrollTop: jQuery("body").offset().top
+        }, 1000 );
+    });
+
+    jQuery(window).scroll(function () {
+        if( jQuery(window).scrollTop() > ( jQuery("#home").outerHeight()-jQuery(".row.menu").outerHeight() ) ){
+            jQuery(".row.menu").addClass("fixed");
+        }
+        if( jQuery(window).scrollTop() <= ( jQuery("#home").outerHeight()-jQuery(".row.menu").outerHeight() ) ){
+            jQuery(".row.menu").removeClass("fixed");            
+        }
+        console.log( jQuery(window).scrollTop() + ">" + jQuery("main > h5").eq(0).offset().top );
+    });
 });
