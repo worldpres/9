27ac7a3e24890e31c897c45632e01324bca7c8b7
dpres <thepressu@gmail.com>
2017-09-23 14:05:13 +0200

    mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,18 @@ jQuery(window).on("load", function () {
     jQuery(".preloader").fadeOut();
 });
 
+
 jQuery(document).ready(function () {
-    jQuery('.carousel').carousel({
-        indicators: true,
-        fullWidth: true,
-        duration: 200,
-        shift: 6,
+
+    jQuery(window).on("scroll load" ,function () {
+        //console.log( jQuery(window).scrollTop() + ">" + jQuery("main > h5").eq(0).offset().top );
+        if( jQuery(window).scrollTop() > ( jQuery("#home").outerHeight()-jQuery(".row.menu").outerHeight() ) ){
+            jQuery(".row.menu").addClass("fixed");
+        }
+        if( jQuery(window).scrollTop() <= ( jQuery("#home").outerHeight()-jQuery(".row.menu").outerHeight() ) ){
+            jQuery(".row.menu").removeClass("fixed");            
+        }
     });
-    setInterval(function () { $('.carousel').carousel('next'); }, 4000);
 
     jQuery(".arrow_down").click(function () {
         jQuery('html, body').animate({
@@ -18,47 +22,39 @@ jQuery(document).ready(function () {
             //nothing :P
         });
     });
-
     jQuery(".arrow_up").click(function () {
         jQuery('html, body').animate({
             scrollTop: jQuery("body").offset().top
         }, 1000 );
     });
 
-    jQuery("#aboutme2").click(function () {
-        jQuery('html, body').animate({
-            scrollTop: jQuery("#aboutme").offset().top-100
-        }, 1000 );
+    jQuery(".menu a").each(function(){
+        $(this).click(function(){
+            jQuery('html, body').animate({
+                scrollTop: jQuery( jQuery(this).attr("href") ).offset().top-60
+            }, 1000 );
+        });
     });
-    jQuery("#works2").click(function () {
-        jQuery('html, body').animate({
-            scrollTop: jQuery("#works").offset().top-100
-        }, 1000 );
+    jQuery(".menu-mobile a").each(function(){
+        $(this).click(function(){
+            jQuery('html, body').animate({
+                scrollTop: jQuery( jQuery(this).attr("href") ).offset().top-60
+            }, 10, function(){
+                jQuery(".menu-mobile").removeClass("show");
+            } );
+        });
     });
-    jQuery("#contact2").click(function () {
-        jQuery('html, body').animate({
-            scrollTop: jQuery("#contact").offset().top-60
-        }, 1000 );
-    });
-    jQuery("#followme2").click(function () {
-        jQuery('html, body').animate({
-            scrollTop: jQuery("#followme").offset().top-100
-        }, 1000 );
-    });
-
-    jQuery("#home2").click(function () {
-        jQuery('html, body').animate({
-            scrollTop: jQuery("#home").offset().top-100
-        }, 1000 );
+    jQuery("#hamburger, #hamburger-mobile").click(function () {
+        jQuery('.menu-mobile').toggleClass("show");
     });
 
-    jQuery(window).on("scroll, load" ,function () {
-        if( jQuery(window).scrollTop() > ( jQuery("#home").outerHeight()-jQuery(".row.menu").outerHeight() ) ){
-            jQuery(".row.menu").addClass("fixed");
-        }
-        if( jQuery(window).scrollTop() <= ( jQuery("#home").outerHeight()-jQuery(".row.menu").outerHeight() ) ){
-            jQuery(".row.menu").removeClass("fixed");            
-        }
-        console.log( jQuery(window).scrollTop() + ">" + jQuery("main > h5").eq(0).offset().top );
+
+    jQuery('.carousel').carousel({
+        indicators: true,
+        fullWidth: true,
+        duration: 200,
+        shift: 6,
     });
+    setInterval(function () { $('.carousel').carousel('next'); }, 4000);
+
 });
